007097e7439036e3e301b22606ef991abb65bd80
onebitlab <onebitresearch@gmail.com>
2026-08-06 21:34:22 +0700

    Remove BRAIN (สมอง) from vocab

--- vocab.js
+++ vocab.js
@@ -1145,7 +1145,7 @@ const VOCAB = [
   { en: 'TOE', trans: 'nio-tao', thai: 'นิ้วเท้า', level: 2, lesson: 8 },
   { en: 'BODY', trans: 'dtuua', thai: 'ตัว', level: 1, lesson: 8 },
   { en: 'BONE', trans: 'gra-duuk', thai: 'กระดูก', level: 1, lesson: 8 },
-  { en: 'BRAIN', trans: 'sa-morng', thai: 'สมอง', level: 2, lesson: 8 },
+
 
   { en: 'KIDNEY', trans: 'dtai', thai: 'ไต', level: 2, lesson: 8 },
 
