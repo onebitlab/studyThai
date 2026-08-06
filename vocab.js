c387596ae10301a11961075cb206693edc087cc9
onebitlab <onebitresearch@gmail.com>
2026-08-06 21:31:48 +0700

    Remove EARRING (ต่างหู) from vocab

--- vocab.js
+++ vocab.js
@@ -1171,7 +1171,7 @@ const VOCAB = [
   { en: 'BELT', trans: 'kem-kat', thai: 'เข็มขัด', level: 2, lesson: 8 },
 
   { en: 'NECKLACE', trans: 'soi', thai: 'สร้อย', level: 1, lesson: 8 },
-  { en: 'EARRING', trans: 'dtaang-huu', thai: 'ต่างหู', level: 1, lesson: 8 },
+
   { en: 'RING', trans: 'waen', thai: 'แหวน', level: 1, lesson: 8 },
   { en: 'BRACELET', trans: 'gam-lai', thai: 'กำไล', level: 2, lesson: 8 },
   { en: 'RIBBON', trans: 'boo', thai: 'โบว์', level: 2, lesson: 8 },
