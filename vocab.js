704351b91ac34ec8134c3ad6999750b9e85b4b7c
onebitlab <onebitresearch@gmail.com>
2026-08-07 13:19:33 +0700

    Remove PAUNCH (พุง) from vocab

--- vocab.js
+++ vocab.js
@@ -1149,7 +1149,7 @@ const VOCAB = [
 
   { en: 'KIDNEY', trans: 'dtai', thai: 'ไต', level: 2, lesson: 8 },
 
-  { en: 'PAUNCH', trans: 'pung', thai: 'พุง', level: 2, lesson: 8 },
+
 
   { en: 'PIMPLE', trans: 'sio', thai: 'สิว', level: 2, lesson: 8 },
   { en: 'TEAR', trans: 'naam-dtaa', thai: 'น้ำตา', level: 2, lesson: 8 },
